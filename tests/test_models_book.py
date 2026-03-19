8ca7ab79b0fefb17e3f87360167a60c2e271c043
LockStockandalotofcode <shivanie0101@gmail.com>
2026-03-19 12:52:32 +0530

    add tests for negative testing in models

--- tests/test_models_book.py
+++ tests/test_models_book.py
@@ -4,3 +4,8 @@ from models.book import Book
 def test_model_book():
     book = Book(book_id=1, title="Don Quixote", author="Miguel de Cervantes")
     assert book.title == "Don Quixote"
+
+def test_model_book_rejects_bad_data():
+    with pytest.raises(Exception):
+        Book(book_id="four", title="Don Quixote")
+    
--- tests/test_models_user.py
+++ tests/test_models_user.py
@@ -4,3 +4,9 @@ from models.user import User
 def test_model_user():
     user = User(user_id= 1, name="Amy")
     assert user.name == "Amy"
+
+def test_model_user_rejects_bad_data():
+    with pytest.raises(Exception):
+        User(user_id="4", name=3)
+        # here string is handled by pydantic but name as int is not converted to string
+    
