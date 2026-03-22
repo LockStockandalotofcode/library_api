7090431267d3f3741fed5920d6b9ac23db87ae05
LockStockandalotofcode <shivanie0101@gmail.com>
2026-03-22 19:32:32 +0530

    fix errors in routes for users

--- routes/users.py
+++ routes/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
-from services.library import LibraryService
+from services.library_service import LibraryService
 
-router = APIRouter(prefix="/users", tag=["Users"])
+router = APIRouter(prefix="/users", tags=["Users"])
 service = LibraryService()
 
 @router.get("/")
@@ -9,16 +9,15 @@ def get_all_users():
     return service.get_all_users()
 
 @router.get("/{user_id}")
-def get_user_by_id():
+def get_user_by_id(user_id: int):
     try:
         return service.get_user_by_id()
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
     
-@router.get("/{user_id/borrowings}")
+@router.get("/{user_id}/borrowings")
 def get_user_borrowings(user_id: int):
     try:
         return service.get_user_borrowings(user_id)
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
-        
